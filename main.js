96746d3a7c723b6e8c31c659e1712f6f57c90baa
CaiqueLudemann <caiqueludemann@gmail.com>
2022-07-03 05:23:14 -0300

    added enter key event in text input field which triggers submit button event, showing alert message.

--- main.js
+++ main.js
@@ -5,12 +5,12 @@ let vowelButton = document.getElementById("button-2");
 let switchCaseButton = document.getElementById("transformCase");
 let caseSwitchCount = 0;
 let randomAnimalCount = 0;
-let animalsArray = ["./resources/dog.webp", "./resources/cat.webp", "./resources/lion.png", "./resources/monkey.png"];
+let animalsArray = ["./resources/dog.webp", "./resources/cat.webp", "./resources/lion.png", "./resources/monkey.jpg"];
 let image = document.getElementById('animalPic');
 let animalInput = document.getElementById('inputAnimal');
 let animalSubmit = document.getElementById('submitAnimal')
-let regEx = /(http:\/\/127.0.0.1:5500\/resources\/)[a-zA-Z]{2}/
-let userAnimalInput = [];
+// let regEx = /(http:\/\/127.0.0.1:5500\/resources\/)[a-zA-Z]{2}/;
+let switchAnimalImage = document.getElementById('switchAnimal');
 
 function randomNumber(){
     return Math.floor(Math.random()*256);
@@ -45,26 +45,37 @@ function switchCase(){
     if (caseSwitchCount % 2 === 0) {
         word1.innerHTML = word1.innerHTML.toLowerCase();
         word2.innerHTML = word2.innerHTML.toLowerCase();        
-        // alert(image.src[32]+image.src[33]+image.src[34])
-        alert(image.src.slice(32,35))
     }
 }
 
 function checkInput(){
-    userAnimalInput.push(animalInput.value)
-    let userInput = animalInput.value.splice(0,3);
-    let imageName = image.src.splice(32,35);
-    let randomAnimal = animalsArray[Math.floor(Math.random()*animalsArray.length)]
+    let userInput = animalInput.value.slice(0,2);
+    let imageName = image.src.slice(32,34);
+    let randomAnimal = animalsArray[Math.floor(Math.random()*animalsArray.length)];
     if (userInput === imageName) {
         image.src = randomAnimal;
+        alert('Good job, Sophia!')
+        animalInput.value='';
     } else {
-        alert('wrong')
+        alert('No, sorry! Love you <3')
     }
 }
 
-console.log(userAnimalInput)
+function changeAnimals(){
+    let randomAnimal = animalsArray[Math.floor(Math.random() * animalsArray.length)]
+    image.src = randomAnimal;
+}
+
+
+animalInput.addEventListener("keypress", function (event) {
+    if (event.key === "Enter") {
+        animalSubmit.click();
+    }
+});
+
 
 consonantButton.onclick = randomConsonant;
 vowelButton.onclick = randomVowel;
 switchCaseButton.onclick = switchCase;
 animalSubmit.onclick = checkInput;
+switchAnimalImage.onclick = changeAnimals
