4965c8b969ce4b03ab817b003bec97571449a72d
CaiqueLudemann <caiqueludemann@gmail.com>
2022-07-03 04:19:51 -0300

    trying to check if userInput.splice(0,3) matches image.src.splice(32,35) in order to return message and switch image.

--- main.js
+++ main.js
@@ -3,6 +3,15 @@ let word2 = document.getElementById('vowel');
 let consonantButton = document.getElementById("button-1");
 let vowelButton = document.getElementById("button-2");
 let switchCaseButton = document.getElementById("transformCase");
+let caseSwitchCount = 0;
+let randomAnimalCount = 0;
+let animalsArray = ["./resources/dog.webp", "./resources/cat.webp", "./resources/lion.png", "./resources/monkey.png"];
+let image = document.getElementById('animalPic');
+let animalInput = document.getElementById('inputAnimal');
+let animalSubmit = document.getElementById('submitAnimal')
+let regEx = /(http:\/\/127.0.0.1:5500\/resources\/)[a-zA-Z]{2}/
+let userAnimalInput = [];
+
 function randomNumber(){
     return Math.floor(Math.random()*256);
 };
@@ -28,11 +37,34 @@ function randomVowel(){
     return word2.innerHTML;
 }
 
-function switchCase(randomConsonant, randomVowel){
+function switchCase(){
+    
     word1.innerHTML = word1.innerHTML.toUpperCase();
     word2.innerHTML = word2.innerHTML.toUpperCase();
+    caseSwitchCount+=1;
+    if (caseSwitchCount % 2 === 0) {
+        word1.innerHTML = word1.innerHTML.toLowerCase();
+        word2.innerHTML = word2.innerHTML.toLowerCase();        
+        // alert(image.src[32]+image.src[33]+image.src[34])
+        alert(image.src.slice(32,35))
+    }
 }
+
+function checkInput(){
+    userAnimalInput.push(animalInput.value)
+    let userInput = animalInput.value.splice(0,3);
+    let imageName = image.src.splice(32,35);
+    let randomAnimal = animalsArray[Math.floor(Math.random()*animalsArray.length)]
+    if (userInput === imageName) {
+        image.src = randomAnimal;
+    } else {
+        alert('wrong')
+    }
+}
+
+console.log(userAnimalInput)
 
 consonantButton.onclick = randomConsonant;
 vowelButton.onclick = randomVowel;
-switchCaseButton.onclick = switchCase
+switchCaseButton.onclick = switchCase;
+animalSubmit.onclick = checkInput;
