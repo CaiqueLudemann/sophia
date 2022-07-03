7b5c3b6199a67e8c38ed6f014478f258ea168e13
CaiqueLudemann <caiqueludemann@gmail.com>
2022-07-03 01:25:58 -0300

    remove redundancy, add random colors to vowel and uppercase button.

--- main.js
+++ main.js
@@ -1,47 +1,38 @@
-let consonantsArray= ['b','c','d','f','g','h','j','k','l','m','n','p','q','r','s','t','v','w','x','y','z'];
-let vowelsArray=['a','e','i','o','u'];
-
+let word1 = document.getElementById('consonant');
+let word2 = document.getElementById('vowel');
+let consonantButton = document.getElementById("button-1");
+let vowelButton = document.getElementById("button-2");
+let switchCaseButton = document.getElementById("transformCase");
+function randomNumber(){
+    return Math.floor(Math.random()*256);
+};
+function randomColor(){
+    return 'rgb(' + randomNumber() + ',' + randomNumber() + ',' + randomNumber() + ')';
+};
 
 
 function randomConsonant(){
-    let randInt = Math.floor(Math.random()*consonantsArray.length);
-    return consonantsArray[randInt]
+    let consonantsArray = ['b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'p', 'q', 'r', 's', 't', 'v', 'w', 'x', 'y', 'z'];
+    let randomIndexNumber = Math.floor(Math.random()*consonantsArray.length);
+    let consonant = consonantsArray[randomIndexNumber];
+    word1.innerHTML= consonant;  
+    return word1.innerHTML;
 }
 
 function randomVowel(){
-    let randInt = Math.floor(Math.random() * vowelsArray.length);
-    return vowelsArray[randInt]
+    let vowelsArray = ['a', 'e', 'i', 'o', 'u'];
+    let randomIndexNumber = Math.floor(Math.random() * vowelsArray.length);
+    let vowel = vowelsArray[randomIndexNumber];
+    word2.innerHTML = vowel;
+    word2.style.color = randomColor();
+    return word2.innerHTML;
 }
 
-
-let consonant = document.getElementById("consonant");
-let vowel = document.getElementById("vowel");
-let consonantButton = document.getElementById("button-1");
-let vowelButton = document.getElementById("button-2");
-let word = document.getElementById('word');
-
-
-function randomNumber(){
-    return Math.floor(Math.random()*256);
+function switchCase(randomConsonant, randomVowel){
+    word1.innerHTML = word1.innerHTML.toUpperCase();
+    word2.innerHTML = word2.innerHTML.toUpperCase();
 }
 
-function joinLetters() {
-    word.innerHTML = consonant.innerHTML + vowel.innerHTML;
-}
-
-function changeConsonant() {
-    consonant.innerHTML = randomConsonant();
-    joinLetters();
-};
-
-function changeVowel() {
-    vowel.innerHTML = randomVowel()
-    vowel.style.color = 'rgb('+randomNumber()+','+randomNumber()+','+randomNumber()+')';
-    joinLetters();
-};
-
-
-
-consonantButton.onclick = changeConsonant;
-vowelButton.onclick = changeVowel;
-
+consonantButton.onclick = randomConsonant;
+vowelButton.onclick = randomVowel;
+switchCaseButton.onclick = switchCase
